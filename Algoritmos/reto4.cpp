a1bfe8e2f881f3361338f0613b94c73aa99f5449
Juan Esteban Ruiz <juanchis17ruiz@gmail.com>
2026-08-04 22:27:12 -0500

    Update reto4.cpp
    Updated output messages for clarity and removed redundant comments.

--- Algoritmos/reto4.cpp
+++ Algoritmos/reto4.cpp
@@ -3,9 +3,8 @@
 using namespace std;
 
 int main() {
-    int usuario, maquina = 1; // La máquina siempre elegirá Piedra (1)
+    int usuario, maquina = 1; 
 
-    cout << "--- PIEDRA, PAPEL O TIJERA ---\n";
     cout << "1. Piedra\n2. Papel\n3. Tijera\n";
     cout << "Elige tu opcion (1-3): ";
     cin >> usuario;
@@ -18,10 +17,10 @@ int main() {
     else if ((usuario == 1 && maquina == 3) || 
              (usuario == 2 && maquina == 1) || 
              (usuario == 3 && maquina == 2)) {
-        cout << "¡Ganaste tú!" << endl;
+        cout << "ganaste" << endl;
     } 
     else {
-        cout << "¡Gana la máquina!" << endl;
+        cout << "perdiste" << endl;
     }
 
     return 0;
