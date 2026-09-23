2e34147f5495ed00eaf20d37da0ea726dee439e9
Juan Esteban Ruiz <juanchis17ruiz@gmail.com>
2026-09-23 09:11:03 -0500

    quiz complejidad.py
    Updated time complexity comments for various algorithms in quiz complejidad.py.

--- Quiz 23sep/quiz complejidad.py
+++ Quiz 23sep/quiz complejidad.py
@@ -1,6 +1,6 @@
-# A. O(n)
-# B. O(n)
-# C. O(n^2)
-# D. O(log n)
+# A. O(1) solo devuelve un elemnto de la lista, revisa un elemento y lo arroja sin mas
+# B. O(n) solo recorre la lista una vez sumando cada que avanza sin necesidad de repetir o hacer mas movimientos
+# C. O(n^2) recorre la lista n veces por el numero de elementos lo que hace que sea exponencial al arrojar n^2 a comparacion de los n elementos
+# D. O(log n) lo divide a la mitad y recoge los datos lo que hace que sea logaritmico normal ya que solo divide y extrae, no recorre las sublistas
 # E. O(n) porq apesar de tener 2 ciclos anidados la funcion solo los recorre, 
 # no hace ningun tipo de operacion y recorre solo una vez para imprimir los datos
