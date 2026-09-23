ebeeb42da2e8b745a28a9437f485b090f6688f37
Juan Esteban Ruiz <juanchis17ruiz@gmail.com>
2026-09-23 09:01:22 -0500

    Add files via upload

---
+++ Quiz 23sep/quiz complejidad.py
@@ -0,0 +1,6 @@
+# A. O(n)
+# B. O(n)
+# C. O(n^2)
+# D. O(log n)
+# E. O(n) porq apesar de tener 2 ciclos anidados la funcion solo los recorre, 
+# no hace ningun tipo de operacion y recorre solo una vez para imprimir los datos
