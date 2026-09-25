78c80efed57bef145a5687719be84fd3aa3e618e
Juan Esteban Ruiz <juanchis17ruiz@gmail.com>
2026-09-25 08:23:07 -0500

    Update comments in 25_sep.py for clarity
    Added comments explaining merge sort and its complexity.

--- 25_sep.py
+++ 25_sep.py
@@ -1,4 +1,5 @@
-#n es las personas que van a entrar al algoritmo (estudiantes, monitores, profes)
-#se ejecuta el doble de veces ya que requiere ver cada usuario por aparte
-#se invocan las secuencias segun el usuario que entre, sin embargo no cambia el proceso
-#listas enlazadas
+#merge sort de una funcion que busca ciertos productos por encima de cierto precio para aplicar un descuento
+#n es la cantidad de productos que esten en la lista con sus respectivos precios O(logn)
+#se ejecuta mas veces al tener que revisar mas elementos tambien existiria la posibilidad de dividir la lista en mas listas por lo que pasa a ser O(nlogn)
+#se invocan mas procesos dentro de la misma funcion, se aplican 2 mas aparte del principal
+#listas enlazadas y arreglos dinamicos
