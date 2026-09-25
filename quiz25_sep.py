b44d8d35302d39fb7b383adfbcf1e6a3cb643359
Juan Esteban Ruiz <juanchis17ruiz@gmail.com>
2026-09-25 08:58:51 -0500

    Update and rename 25_sep.py to quiz25_sep.py

--- quiz25_sep.py
+++ quiz25_sep.py
@@ -3,3 +3,6 @@
 #se ejecuta mas veces al tener que revisar mas elementos tambien existiria la posibilidad de dividir la lista en mas listas por lo que pasa a ser O(nlogn)
 #se invocan mas procesos dentro de la misma funcion, se aplican 2 mas aparte del principal
 #listas enlazadas y arreglos dinamicos
+#el codigo que elegi solo toma datos y no genera ningun tipo de proceso, por lo que el codigo en las 3 funciones es O(1) haciendo que la complejidad del codigo sea costante, osea 0
+#en el mejor caso ningun producto supera el precio por lo que no siguen haciendo la siguiente funcion
+#el peor caso es muchos precios superan el limite y tiene que invocar todas las funciones del codigo sin embargo ninguna complejidad en las funciones supera la complejidad O(1)
