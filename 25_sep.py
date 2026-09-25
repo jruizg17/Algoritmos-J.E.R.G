bbc6cbad815345b6c23954eddf274bd100740f2b
Juan Esteban Ruiz <juanchis17ruiz@gmail.com>
2026-09-25 08:10:40 -0500

    Add files via upload

---
+++ 25_sep.py
@@ -0,0 +1,4 @@
+#n es las personas que van a entrar al algoritmo (estudiantes, monitores, profes)
+#se ejecuta el doble de veces ya que requiere ver cada usuario por aparte
+#se invocan las secuencias segun el usuario que entre, sin embargo no cambia el proceso
+#listas enlazadas
